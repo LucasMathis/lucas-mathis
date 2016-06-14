1ab7840cce6db7f099ea3c3101eaabca0c7e3fc2
LucasMathis <mathis.lucas10@gmail.com>
2016-06-14 15:30:35 -0700

    Working email functionality, copyright added
    Added copyright, CSS changes throughout, working email functionality.

--- sendMyMail.php
+++ sendMyMail.php
@@ -1,6 +1,14 @@
 <?php
 
+$sendTo = "mathis.lucas10@gmail.com";
+$sendersName =  stripslashes(htmlentities($_POST["sendersName"]));
+$sendersEmail = stripslashes(htmlentities($_POST["sendersEmail"]));
+$emailMsg =     stripslashes(htmlentities($_POST["emailMsg"]));
 
 
+$emailMsg = $sendersName." ".$sendersEmail." ".$emailMsg;
 
+mail($sendTo,"Portfolio Message",$emailMsg);
+                             
+print "<meta http-equiv=\"refresh\" content=\"0;URL=contact.html\">";
 ?>
