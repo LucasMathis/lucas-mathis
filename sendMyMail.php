771ecb159f9971d21e1d62fe47c524f95744500a
LucasMathis <mathis.lucas10@gmail.com>
2016-11-21 13:26:35 -0800

    Added working captcha
    Added captcha, Needs elegant success and failure pages.

--- sendMyMail.php
+++ sendMyMail.php
@@ -1,24 +1,49 @@
 <?php
 
-$captcha;
 
-if(isset($_POST['g-recaptcha-response'])){
-          $captcha=$_POST['g-recaptcha-response'];
-        }
-
-if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
-          exit;
-        }
-        $secretKey = "6Lf3aQwUAAAAAPjkk-ssEeoL7IGkojX1zBh4-Rn";
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
-        $responseKeys = json_decode($response,true);
-        if(intval($responseKeys["success"]) !== 1) {
-          echo '<h2>Sorry, unable to send message. Please try again later./h2>';
-        } else {
-          echo '<h2>Thanks for sending a message!</h2>';
-        }
+if(isset($_POST['submit']) && !empty($_POST['submit'])):
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+        //your site secret key
+        $secret = '6Lf3aQwUAAAAAPjkk-ssEeoL7IGkojX1zBh4-Rna';
+        //get verify response data
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success):
+            //contact form submission code
+			$sendersName =  stripslashes(htmlentities($_POST["sendersName"]));
+			$sendersEmail = stripslashes(htmlentities($_POST["sendersEmail"]));
+			$emailMsg =     stripslashes(htmlentities($_POST["emailMsg"]));
+            
+            $to = 'mathis.lucas10@gmail.com';
+            $subject = 'Contact from Portfolio Website';
+            $htmlContent = "
+                <h1>Contact request details</h1>
+                <p><b>Name: </b>".$sendersName."</p>
+                <p><b>Email: </b>".$sendersEmail."</p>
+                <p><b>Message: </b>".$emailMsg."</p>
+            ";
+            // Always set content-type when sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            // More headers
+            $headers .= 'From:'.$sendersName.' <'.$sendersEmail.'>' . "\r\n";
+            //send email
+            mail($to,$subject,$htmlContent,$headers);
+            
+            $succMsg = 'Your contact request have submitted successfully.';
+            echo $succMsg;
+        else:
+            $errMsg = 'Robot verification failed, please try again.';
+        	echo $errMsg;
+        endif;
+    else:
+        $errMsg = 'Please click on the reCAPTCHA box.';
+    	echo $errMsg;
+    endif;
+else:
+    $errMsg = '';
+    $succMsg = '';
+endif;
 
 
 
